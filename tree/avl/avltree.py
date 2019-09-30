1321541630592001b967b702206ea6a6937c4733
Junwon Jung <junwon3329@gmail.com>
2019-09-30 14:08:53 -0500

    Delete implementation completed

--- tree/avl/avltree.py
+++ tree/avl/avltree.py
@@ -61,7 +61,6 @@ class AVLTree:
                 return self.left_rotate(root)
 
             return root
-
         self.root = insert_helper(self.root, key)
 
     def left_rotate(self, root):
@@ -103,14 +102,76 @@ class AVLTree:
 
         print_helper(self.root)
 
+    def delete(self, key):
+
+        def delete_helper(root, key):
+            if not root:
+                return None
+            if root.key < key:
+                root.right = delete_helper(root.right, key)
+            elif root.key > key:
+                root.left = delete_helper(root.left, key)
+            else:
+                # decrease if count is more than 1
+                if root.count > 1:
+                    root.count -= 1
+                    return root
+
+                if not root.left:
+                    return root.right
+                elif not root.right:
+                    return root.left
+                else:
+                    min_node = self.get_minimum_node(root.right)
+                    root.key = min_node.key
+                    root.count = min_node.count
+                    # make min node count == 1 so that it can be deleted right away
+                    min_node.count = 1
+                    root.right = delete_helper(root.right, min_node.key)
+
+            self.set_height(
+                root, 1 + max(self.get_height(root.left), self.get_height(root.right)))
+
+            balance = self.get_balance(root)
+
+            # if balance > 0 then it means that left height is longer than right by how long
+            # left right and left left
+            if balance > 1:
+                if self.get_balance(root.left) < 0:
+                    root.left = self.left_rotate(root.left)
+                return self.right_rotate(root)
+
+            elif balance < -1:
+                if self.get_balance(root.right) > 0:
+                    root.right = self.right_rotate(root.right)
+                return self.left_rotate(root)
+
+            return root
+
+        self.root = delete_helper(self.root, key)
+
+    def get_minimum_node(self, root):
+        if not root or not root.left:
+            return root
+        return self.get_minimum_node(root.left)
+
 
 avl = AVLTree()
 
-nums = [random.randint(0, 500) for _ in range(1000)]
+nums = [random.randint(0, 10) for _ in range(10)]
 
-# nums = [9, 5, 10, 0, 6, 11, -1, 1, 2]
+# nums = [9, 5, 10, 0, 6, 11, -1, 1, 2,1,1,1,1,1]
 
 for num in nums:
     avl.insert(num)
+
+# avl.print()
+
+print(nums)
+
+print(nums[:len(nums) // 2])
+
+for num in nums[:len(nums) // 2]:
+    avl.delete(num)
 
 avl.print()
